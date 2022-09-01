f1a55fd4528cd54dc1780ee7c2b6d54a41abdc14
jukte-kz <kanat.jukte@gmail.com>
2022-09-01 11:44:44 +0600

    fix: register disable

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5c549a5154540b2"; }
+/******/ 		__webpack_require__.h = function() { return "c4cbe82fdfaa5d1d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
