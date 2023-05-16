0d7618e8e87d6a3bc17f1f202347dac4a8ff45b3
b.kuandyk <b.kuandyk@mycar.kz>
2023-05-17 05:34:47 +0600

    fix: remove unused attribute

--- components/organisms/Registration/index.tsx
+++ components/organisms/Registration/index.tsx
@@ -251,7 +251,6 @@ export const RegistrationView = () => {
           inputClass={"w-100"}
           excludeCountries={['ru']}
           country={'kz'}
-          excludeCountries={['ru']}
           value={phone}
           onChange={phone => {
             setPhone(phone);
